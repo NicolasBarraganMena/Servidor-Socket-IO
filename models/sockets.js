22bbfc3ae55fd10eda882342c3e33211f1254a5b
Gamba <juanca.quinterom@gmail.com>
2021-11-01 15:58:34 -0500

    Limpiar sockets.js

--- models/sockets.js
+++ models/sockets.js
@@ -7,6 +7,7 @@ const { json } = require('express');
 
 class Sockets
 {
+    // Start
     constructor(io)
     {
         this.io = io;
@@ -17,28 +18,10 @@ class Sockets
         this.lookForMatchReady();
     }
 
-    socketMiddlewares()
-    {
-        //Validar token
-        this.io.use((socket,next)=>
-        {
-            let {token} = socket.handshake.query;
-            let uid = comprobarJWT(token);
-            if(uid)
-            {
-                socket.uid = uid;
-                return next();
-            }
-
-            //Emitir desconexion por falta de autorizacion
-            console.log("reject token");
-            return next(new Error("authentication error"));
-        });
-    }
-
+    // Server Events
     socketsEvents()
     {
-        this.io.on('connection', async (socket) =>
+        io.on('connection', async socket =>
         {
             console.log('a user connected ' + socket.uid);
 
@@ -57,14 +40,16 @@ class Sockets
             for (const friend of user.friends)
             {
                 let connected = this.onlineUsers.find(u => u.username == friend.username);
-                if(connected){
-                    this.io.sockets.connected[connected.id].emit('user-connected',{username:user.username}) ;
+
+                if(connected)
+                {
+                    this.io.sockets.connected[connected.id].emit('user-connected',{username:user.username});
                     console.log(connected.id);
                 }
             }
 
             //Evento Find Match
-            socket.on('findMatch',()=>
+            socket.on('findMatch',() =>
             {
                 let user = this.onlineUsers.find(u => u.id == socket.id);
                 console.log(`${user.username} esta buscando partida`);
@@ -72,7 +57,7 @@ class Sockets
             });
 
             //Evento Chat Global
-            socket.on('chat',(msg)=>
+            socket.on('chat', msg =>
             {
                 let user = this.onlineUsers.find(u => u.id == socket.id);
                 
@@ -86,7 +71,7 @@ class Sockets
             });
             
             //Evento Chat Privado
-            socket.on('privateChat',(msg)=>
+            socket.on('privateChat', msg =>
             {
                 let user = this.onlineUsers.find(u => u.id == socket.id);
                 let targetUser = this.onlineUsers.find(u => u.username == msg.target);
@@ -105,10 +90,11 @@ class Sockets
             });
 
             //Evento para solicitud de amistad
-            socket.on('requestFriendship',(username)=>
+            socket.on('requestFriendship', username =>
             {
                 let connected = this.onlineUsers.find(u => u.username == username);
-                if(connected){
+                if(connected)
+                {
                     //Emit friendRequest
                     console.log(connected.id);        
                 }
@@ -128,6 +114,38 @@ class Sockets
         });
     }
     
+    socketMiddlewares()
+    {
+        //Validar token
+        this.io.use((socket, next) =>
+        {
+            let {token} = socket.handshake.query;
+            let uid = comprobarJWT(token);
+            if(uid)
+            {
+                socket.uid = uid;
+                return next();
+            }
+
+            //Emitir desconexion por falta de autorizacion
+            console.log("reject token");
+            return next(new Error("authentication error"));
+        });
+    }
+
+    lookForMatchReady()
+    {
+        setInterval(()=>
+        {
+            if(matchmaking.startingMatches.length>0)
+            {
+                let matchReady = matchmaking.startingMatches.shift();
+                console.log("Starting Match: "+matchReady.id);
+                this.startMatchReady(matchReady);
+            }
+        }, 1000);
+    }
+    
     startMatchReady(match)
     {
         let room = match.id;
@@ -144,20 +162,6 @@ class Sockets
             match: match.id
         });
      }
-
-    lookForMatchReady()
-    {
-        setInterval(()=>
-        {
-            if(matchmaking.startingMatches.length>0)
-            {
-                
-                let matchReady = matchmaking.startingMatches.shift();
-                console.log("Starting Match: "+matchReady.id);
-                this.startMatchReady(matchReady);
-            }
-        },1000);
-    }
 }
 
 module.exports = Sockets;
