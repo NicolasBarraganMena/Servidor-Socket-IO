cef0f45e13d9fe565bf88de5761751551e01006d
Gamba <juanca.quinterom@gmail.com>
2021-11-01 15:48:00 -0500

    Update sockets.js

--- models/sockets.js
+++ models/sockets.js
@@ -5,30 +5,41 @@ const Usuario = require('../models/usuario');
 const { usuarioConectado } = require('../controllers/sockets');
 const { json } = require('express');
 
-class Sockets{
-    constructor(io){
+class Sockets
+{
+    constructor(io)
+    {
         this.io = io;
         this.onlineUsers = [];
+        this.rooms = [];
         this.socketsEvents();
         this.socketMiddlewares();
         this.lookForMatchReady();
     }
-    socketMiddlewares(){
+
+    socketMiddlewares()
+    {
         //Validar token
-        this.io.use((socket,next)=> {
+        this.io.use((socket,next)=>
+        {
             let {token} = socket.handshake.query;
             let uid = comprobarJWT(token);
-            if(uid){
+            if(uid)
+            {
                 socket.uid = uid;
                 return next();
             }
+
             //Emitir desconexion por falta de autorizacion
             console.log("reject token");
             return next(new Error("authentication error"));
         });
     }
-    socketsEvents(){
-        this.io.on('connection', async (socket) => {
+
+    socketsEvents()
+    {
+        this.io.on('connection', async (socket) =>
+        {
             console.log('a user connected ' + socket.uid);
 
             let {password, _id:uid , ...user} = await Usuario.findById(socket.uid);
@@ -43,22 +54,26 @@ class Sockets{
             socket.emit('onConnection', { username: user.username});
             
             //Notificar conexion a los amigos
-            for (const friend of user.friends) {
+            for (const friend of user.friends)
+            {
                 let connected = this.onlineUsers.find(u => u.username == friend.username);
                 if(connected){
                     this.io.sockets.connected[connected.id].emit('user-connected',{username:user.username}) ;
                     console.log(connected.id);
                 }
-            }        
+            }
+
             //Evento Find Match
-            socket.on('findMatch',()=>{
+            socket.on('findMatch',()=>
+            {
                 let user = this.onlineUsers.find(u => u.id == socket.id);
                 console.log(`${user.username} esta buscando partida`);
                 matchmaking.addPlayer(user);
             });
 
             //Evento Chat Global
-            socket.on('chat',(msg)=>{
+            socket.on('chat',(msg)=>
+            {
                 let user = this.onlineUsers.find(u => u.id == socket.id);
                 
                 console.log(`${user.username} esta enviando un mensaje: ` + msg.message);
@@ -71,7 +86,8 @@ class Sockets{
             });
             
             //Evento Chat Privado
-            socket.on('privateChat',(msg)=>{
+            socket.on('privateChat',(msg)=>
+            {
                 let user = this.onlineUsers.find(u => u.id == socket.id);
                 let targetUser = this.onlineUsers.find(u => u.username == msg.target);
                 
@@ -89,15 +105,18 @@ class Sockets{
             });
 
             //Evento para solicitud de amistad
-            socket.on('requestFriendship',(username)=>{
+            socket.on('requestFriendship',(username)=>
+            {
                 let connected = this.onlineUsers.find(u => u.username == username);
                 if(connected){
                     //Emit friendRequest
                     console.log(connected.id);        
                 }
             });
+
             //Evento cuando se desconecta un cliente
-            socket.on('disconnect', () => {
+            socket.on('disconnect', () =>
+            {
                 this.onlineUsers = this.onlineUsers.filter(u => u.id != socket.id);
                 /*
                 AQUI DEBERIA VERIFICAR SI EL USUARIO DESCONECTADO
@@ -107,25 +126,31 @@ class Sockets{
                 usuarioConectado(user.uid,false);         
             });
         });
-        
     }
     
-    startMatchReady(match){
+    startMatchReady(match)
+    {
         let room = match.id;
-        for (const player of match.players) {
+        for (const player of match.players)
+        {
              player.room = room;
              console.log(player.id);
              this.io.sockets.connected[player.id].join(room);        
-        } 
-        this.io.to(room).emit('matchReady',{
+        }
+
+        this.io.to(room).emit('matchReady',
+        {
             msg:"Match Ready",
             match: match.id
         });
      }
 
-    lookForMatchReady(){
-        setInterval(()=>{
-            if(matchmaking.startingMatches.length>0){
+    lookForMatchReady()
+    {
+        setInterval(()=>
+        {
+            if(matchmaking.startingMatches.length>0)
+            {
                 
                 let matchReady = matchmaking.startingMatches.shift();
                 console.log("Starting Match: "+matchReady.id);
